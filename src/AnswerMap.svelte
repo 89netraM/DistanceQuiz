94abbfcbda4b806d8c66a23409cfcb32d43803ab
Mårten Åsberg <marten.asberg@accenture.com>
2024-04-01 16:39:06 +0200

    Something to start with

---
+++ src/AnswerMap.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import "leaflet/dist/leaflet.css";
+	import { Map, LatLngBounds, LatLng, TileLayer, Tooltip, Point } from "leaflet";
+	import { GeodesicLine } from "leaflet.geodesic";
+
+	export let from: LatLng;
+	export let targets: ReadonlyArray<LatLng>;
+	export let mapLayer: TileLayer;
+	export let fromIcon: string = "🔷";
+	export let targetIcon: string = "🔶";
+	export let goalIcon: string = "⭐";
+
+	function LoadMap(node: HTMLElement): void {
+		const map = new Map(node);
+		map.addLayer(mapLayer);
+
+		const bound = new LatLngBounds([from, ...targets]);
+		map.fitBounds(bound);
+
+		for (const target of targets) {
+			addTarget(map, from, target);
+		}
+
+		addPoint(map, from, fromIcon);
+	}
+
+	function addTarget(map: Map, from: LatLng, target: LatLng): void {
+		addLine(map, from, target);
+		addPoint(map, target, targetIcon);
+		addDistance(map, from, target);
+	}
+
+	function addPoint(map: Map, point: LatLng, icon: string): void {
+		map.addLayer(
+			new Tooltip(point, {
+				content: icon,
+				permanent: true,
+				direction: "center",
+				opacity: 1,
+				className: "target-icon",
+			}),
+		);
+	}
+
+	function addLine(map: Map, from: LatLng, target: LatLng): void {
+		map.addLayer(
+			new GeodesicLine([from, target], {
+				weight: 2,
+				steps: 8,
+				wrap: true,
+			}),
+		);
+	}
+
+	function addDistance(map: Map, from: LatLng, target: LatLng): void {
+		const distance = map.distance(from, target);
+		map.addLayer(
+			new Tooltip(target, {
+				content: humanize(distance),
+				permanent: true,
+				direction: "center",
+				opacity: 1,
+				className: "distance-label",
+				offset: new Point(0, 30),
+			}),
+		);
+	}
+
+	function humanize(length: number): string {
+		if (length >= 10_000) {
+			return `${Math.round(length / 1_000)} km`;
+		}
+		return `${Math.round(length)} m`;
+	}
+</script>
+
+<div use:LoadMap />
+
+<style lang="scss">
+	div {
+		width: 100%;
+		height: 100%;
+	}
+
+	:global(.target-icon) {
+		background: none;
+		border: none;
+		box-shadow: none;
+		font-size: 1.5rem;
+	}
+	:global(.distance-label) {
+		background-color: rgba(255, 255, 255, 0.75);
+		padding: 3px 6px;
+	}
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { LatLng, TileLayer } from "leaflet";
+	import AnswerMap from "./AnswerMap.svelte";
+</script>
+
+<main>
+	<AnswerMap
+		mapLayer={new TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+			maxZoom: 19,
+		})}
+		from={new LatLng(57.616227, 11.775967)}
+		targets={[
+			new LatLng(57.618611, 11.768512),
+			new LatLng(57.609116, 11.784028),
+			new LatLng(57.61226, 11.790719),
+			new LatLng(57.616056, -11.794048),
+		]}
+	/>
+</main>
+
+<style lang="scss">
+	main {
+		width: 100%;
+		height: 100%;
+	}
+</style>
