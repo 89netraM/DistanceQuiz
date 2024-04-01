2a597cf2970994ca1c1c3b84f65bbe06784024d5
Mårten Åsberg <marten.asberg@outlook.com>
2024-04-01 17:47:30 +0200

    Question view

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
-	import { LatLng, TileLayer } from "leaflet";
-	import AnswerMap from "./AnswerMap.svelte";
+	import QuestionView from "./QuestionView.svelte";
 </script>
 
 <main>
-	<AnswerMap
-		mapLayer={new TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-			maxZoom: 19,
-		})}
-		from={new LatLng(57.616227, 11.775967)}
-		targets={[
-			new LatLng(57.618611, 11.768512),
-			new LatLng(57.609116, 11.784028),
-			new LatLng(57.61226, 11.790719),
-			new LatLng(57.616056, 11.794048),
+	<QuestionView
+		options={[
+			"https://åsberg.net/me.jpg",
+			"https://åsberg.net/denmark.jpg",
+			"https://åsberg.net/me.jpg",
+			"https://åsberg.net/denmark.jpg",
 		]}
 	/>
 </main>
---
+++ src/QuestionView.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let options: ReadonlyArray<string>;
+</script>
+
+<div>
+	{#each options as option}
+		<img src={option} />
+	{/each}
+</div>
+
+<style lang="scss">
+	div {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-content: center;
+
+		> img {
+			flex-grow: 1;
+			flex-basis: 50%;
+			max-width: 50%;
+			max-height: 50%;
+			object-fit: cover;
+		}
+	}
+</style>
